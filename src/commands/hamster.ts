0e04184a6688de627f3413359bacdadf9d1965f1
Roby Tanama <robytanma@gmail.com>
2024-05-02 01:53:46 +0800

    feat: finish hamster command

---
+++ src/commands/hamster.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { config } from "../config";
+import { get } from "lodash";
+
+const COMMAND_NAME = "hamster";
+
+export const data = new SlashCommandBuilder()
+  .setName(COMMAND_NAME)
+  .setDescription("Sends a random picture of a hamster");
+
+export const execute = async (interaction: CommandInteraction) => {
+  const { data } = await axios.get(
+    "https://api.night-api.com/images/animals/hamster",
+    {
+      headers: {
+        Authorization: config.NIGHT_API_KEY,
+      },
+    }
+  );
+  interaction.reply(get(data, "content.url"));
+};
