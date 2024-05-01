99b796f42ae7def37ef029be41101fb3fb346a03
Vocrea <125andreahoziwei@gmail.com>
2024-05-02 02:14:37 +0800

    feat: add command for ten_queens

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -8,10 +8,14 @@
 // Import the "ping" command module
 import * as ping from "./ping";
 
+//Import the "ten_queens" command
+import * as ten_queens from "./ten_queens";
+
 /**
  * Object containing all available commands.
  * Each property corresponds to a command module.
  */
 export const commands = {
-  ping,
+    ping,
+    ten_queens,
 };
---
+++ src/commands/ten_queens.ts
@@ -0,0 +1,29 @@
+/**
+ * ten_queens.ts
+ *
+ * This file contains the definition of the "/ten_queens" command for a Discord bot.
+ * The command responds with "Hell No!" when invoked.
+ * It utilizes the Discord.js library for interacting with the Discord API.
+ */
+
+import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+
+//Name of command
+const COMMAND_NAME = "ten_queens";
+
+/**
+ * Represents the data required to define the "ten_queens" command.
+ * This includes the command name and description.
+ */
+export const data = new SlashCommandBuilder()
+    .setName(COMMAND_NAME)
+    .setDescription("Replies with Hell No!");
+
+/**
+ * Executes the "ten_queens" command.
+ * @param {CommandInteraction} interaction - The interaction event triggered by the command.
+ */
+export const execute = async (interaction: CommandInteraction) => {
+    // Reply to the interaction with "Hell No!"
+    interaction.reply("Hell No!");
+};
