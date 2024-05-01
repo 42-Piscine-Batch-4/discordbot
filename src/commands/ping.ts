c9e5b27d849d317055c1a7ce01b4cdc39bb17ad3
Roby Tanama <robytanma@gmail.com>
2024-05-01 18:33:19 +0800

    feat: add ping.ts command

---
+++ src/commands/ping.ts
@@ -0,0 +1,29 @@
+/**
+ * pingCommand.ts
+ *
+ * This file contains the definition of a simple "ping" command for a Discord bot.
+ * The command responds with "PONG!" when invoked.
+ * It utilizes the Discord.js library for interacting with the Discord API.
+ */
+
+import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+
+// Name of the command
+const COMMAND_NAME = "ping";
+
+/**
+ * Represents the data required to define the "ping" command.
+ * This includes the command name and description.
+ */
+export const data = new SlashCommandBuilder()
+  .setName(COMMAND_NAME)
+  .setDescription("Replies with PONG!");
+
+/**
+ * Executes the "ping" command.
+ * @param {CommandInteraction} interaction - The interaction event triggered by the command.
+ */
+export const execute = async (interaction: CommandInteraction) => {
+  // Reply to the interaction with "PONG!"
+  interaction.reply("PONG!");
+};
