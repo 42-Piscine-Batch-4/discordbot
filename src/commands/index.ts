c645aee9b25b20f009957fd142bcee3d5c29ed15
Baixier <151296638+Baixier@users.noreply.github.com>
2024-05-03 17:35:22 +0800

    added sort_asc implementation additional separator feature

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -17,8 +17,9 @@ import * as ping from "./ping";
 // Import the "rev" command
 import * as rev from "./rev";
 
-// Import the "sort_desc" command
+// Import the "sort_desc" and "sort_asc" commands
 import * as sort_desc from "./sort_desc";
+import * as sort_asc from "./sort_asc";
 
 // Import the "ten_queens" command
 import * as ten_queens from "./ten_queens";
@@ -36,6 +37,7 @@ export const commands = {
   ping,
   rev,
   sort_desc,
+  sort_asc,
   ten_queens,
   dice,
 };
--- src/commands/sort_desc.ts
+++ src/commands/sort_desc.ts
@@ -3,21 +3,24 @@ import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 const COMMAND_NAME = "sort_desc";
 
 export const data = new SlashCommandBuilder()
-    .setName(COMMAND_NAME)
-    .setDescription("Sorts list of integers in descending order.")
-    .addStringOption(option =>
-        option
-        .setName("input")
-        .setDescription("raw user input.")
-        .setRequired(true));
+  .setName(COMMAND_NAME)
+  .setDescription("Sorts list of integers in descending order.")
+  .addStringOption((option) =>
+    option.setName("input").setDescription("raw user input.").setRequired(true)
+  );
 
 export const execute = async (interaction: ChatInputCommandInteraction) => {
-    const input: String = interaction.options.getString("input");
-    const strsplit = input.split(/ |\t|\f|\n|\r|\v/).filter(c => c).map(c => parseInt(c));
-    if (strsplit.includes(NaN)) {
-      await interaction.reply("Invalid argument: only digits!");
-    } else {
-      const finum = strsplit.slice().sort((a, b) =>  b - a);
-      await interaction.reply(`initial list: ${strsplit}\nsorted array: ${finum}`);
-    }
+  const input: String = interaction.options.getString("input");
+  const strsplit = input
+    .split(/ |\t|\f|\n|\r|\v/)
+    .filter((c) => c)
+    .map((c) => parseInt(c));
+  if (strsplit.includes(NaN)) {
+    await interaction.reply("Invalid argument: only digits!");
+  } else {
+    const finum = strsplit.slice().sort((a, b) => b - a);
+    await interaction.reply(
+      `initial list: ${strsplit}\nsorted array: ${finum}`
+    );
+  }
 };
