8a12059d206f4812cb52bbc769aa592437a74317
Heixier <rzsiah@gmail.com>
2024-05-09 06:27:06 +0800

    fixed bedtime check

--- src/commands/bedtime.ts
+++ src/commands/bedtime.ts
@@ -6,7 +6,7 @@ const COMMAND_NAME = "bedtime"
 // Sets the start and end timings here for easier access in future
 type Time = { hour: number; minute: number }
 
-const startTime: Time = { hour: 23, minute: 42 }
+const startTime: Time = { hour: 8, minute: 42 }
 const endTime: Time = { hour: 7, minute: 42 }
 
 export const data = new SlashCommandBuilder()
@@ -33,19 +33,19 @@ export const execute = async (interaction: ChatInputCommandInteraction) => {
     .set("hour", endTime.hour)
     .set("minute", endTime.minute)
 
-  /**
-   * This is not accounting properly for past-midnight checks
-   */
-  const isBedTime =
-    (currentTime.isAfter(bedTime) && currentTime.isBefore(wakeTime)) ||
-    (currentTime.isAfter(wakeTime) &&
-      currentTime.isBefore(bedTime.add(1, "day")))
+  const isBedTime = !(
+    currentTime.isAfter(wakeTime) && currentTime.isBefore(bedTime.add(1, "day"))
+  )
+  const bedHoursDiff = Math.abs(currentTime.diff(bedTime, "hours"))
+  const bedMinutesDiff = Math.abs(currentTime.diff(bedTime, "minutes") % 60)
+
+  // this is wrong. diff doesn't check direction properly. if startTime is 8:42 and endTime is 7:42 it thinks there's 1 hour left to sleep
+  const wakeHoursDiff = Math.abs(currentTime.diff(wakeTime, "hours"))
+  const wakeMinutesDiff = Math.abs(currentTime.diff(wakeTime, "minutes") % 60)
 
   if (!isBedTime) {
-    const hoursDiff = Math.abs(currentTime.diff(bedTime, "hours"))
-    const minutesDiff = Math.abs(currentTime.diff(bedTime, "minutes") % 60)
     interaction.reply(
-      `It's still ${hoursDiff <= 1 ? (hoursDiff === 0 ? "" : hoursDiff + " hour and ") : hoursDiff + " hours and "}${minutesDiff} minutes before bed!`
+      `It's still ${bedHoursDiff <= 1 ? (bedHoursDiff === 0 ? "" : bedHoursDiff + " hour and ") : bedHoursDiff + " hours and "}${bedMinutesDiff} minutes before bed!`
     )
   } else {
     if (interaction.options.getString("defy")) {
@@ -54,7 +54,7 @@ export const execute = async (interaction: ChatInputCommandInteraction) => {
       )
     } else {
       interaction.reply(
-        `It's past ${bedTime.hour() % 12}:${bedTime.minute()} ${bedTime.hour() > 12 ? "pm" : "am"}, time to sleep.`
+        `It's past ${bedTime.hour() % 12}:${bedTime.minute()} ${bedTime.hour() > 12 ? "pm" : "am"}. You have ${wakeHoursDiff <= 1 ? (wakeHoursDiff === 0 ? "" : wakeHoursDiff + " hour and ") : wakeHoursDiff + " hours and "}${wakeMinutesDiff} more minutes left to sleep.`
       )
     }
   }
