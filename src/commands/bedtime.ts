814cb82b3069c449c23d032e60ae48bb3b760be0
Heixier <rzsiah@gmail.com>
2024-05-08 18:35:43 +0800

    added defy option to defy bedtime

--- src/commands/bedtime.ts
+++ src/commands/bedtime.ts
@@ -1,26 +1,47 @@
-import { CommandInteraction, SlashCommandBuilder } from "discord.js"
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js"
 import mydayjs from "../utils/dayjs"
 
 const COMMAND_NAME = "bedtime"
 
+// Sets the start and end timings here for easier access in future
+type Time = { hour: number; minute: number }
+
+const startTime: Time = { hour: 23, minute: 42 }
+const endTime: Time = { hour: 7, minute: 42 }
+
 export const data = new SlashCommandBuilder()
   .setName(COMMAND_NAME)
   .setDescription("Checks if it's bedtime")
+  .addBooleanOption((option) =>
+    option.setName("defy").setDescription("Defies bedtime").setRequired(false)
+  )
 
-/**
- * Executes the "ping" command.
- * @param {CommandInteraction} interaction - The interaction event triggered by the command.
- */
-export const execute = async (interaction: CommandInteraction) => {
+export const execute = async (interaction: ChatInputCommandInteraction) => {
   const currentTime = mydayjs().tz()
 
-  const bedTime = mydayjs().tz().set("hour", 23).set("minute", 42)
+  const bedTime = mydayjs()
+    .tz()
+    .set("hour", startTime.hour)
+    .set("minute", startTime.minute)
 
-  const wakeTime = mydayjs().tz().set("hour", 7).set("minute", 42)
+  const wakeTime = mydayjs()
+    .tz()
+    .set("hour", endTime.hour)
+    .set("minute", endTime.minute)
 
   if (currentTime < bedTime && currentTime > wakeTime) {
-    interaction.reply(`It's still too early for bed!`)
+    interaction.reply(
+      `It's still ${Math.abs(currentTime.hour() - bedTime.hour())} hours and ${Math.abs(currentTime.minute() - bedTime.minute())} minutes before bed!`
+    )
   } else {
-    interaction.reply(`It's past 11.42pm, time to sleep.`)
+    if (interaction.options.getBoolean("defy")) {
+      interaction.reply(
+        `It's past ${startTime.hour % 12}:${startTime.minute} pm but I don't care!!!`
+      )
+    } else {
+      interaction.reply(
+        `It's past ${startTime.hour % 12}:${startTime.minute} pm, time to sleep.`
+      )
+    }
   }
 }
