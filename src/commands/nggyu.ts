965f869e39331233c5b768f622c4701d8ec2a6ee
rbd03 <raydenleong03@gmail.com>
2024-05-08 18:58:28 +0800

    feat: add /nggyu command.

---
+++ src/commands/nggyu.ts
@@ -0,0 +1,45 @@
+import {
+  createAudioPlayer,
+  createAudioResource,
+  joinVoiceChannel,
+} from "@discordjs/voice"
+import {
+  ChatInputCommandInteraction,
+  GuildMember,
+  SlashCommandBuilder,
+} from "discord.js"
+import ytdl from "ytdl-core"
+import client from "../index"
+
+const COMMAND_NAME = "nggyu"
+
+export const data = new SlashCommandBuilder()
+  .setName(COMMAND_NAME)
+  .setDescription("Adds the bot to voice channel and plays NGGYU.")
+
+export const execute = async (interaction: ChatInputCommandInteraction) => {
+  const stream = ytdl("https://www.youtube.com/watch?v=dQw4w9WgXcQ", {
+    filter: "audioonly",
+  })
+  const player = createAudioPlayer()
+  const resource = createAudioResource(stream)
+  const member = interaction.member
+  if (member) {
+    const channel = (member as GuildMember).voice.channel
+    if (channel) {
+      const connection = joinVoiceChannel({
+        channelId: channel.id,
+        guildId: channel.guild.id,
+        adapterCreator: channel.guild.voiceAdapterCreator,
+      })
+      connection.subscribe(player)
+      player.play(resource)
+      client.on("voiceStateUpdate", (_, newState) => {
+        if (newState.channelId == null) {
+          connection.disconnect()
+        }
+      })
+    }
+  }
+  await interaction.reply("Invalid command!")
+}
