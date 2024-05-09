63ac36e25a705a26868e330d7548b8028367b78a
rbd03 <raydenleong03@gmail.com>
2024-05-09 18:55:16 +0800

    style: rearrange intents in alphabetical order.

--- src/client.ts
+++ src/client.ts
@@ -2,11 +2,11 @@ import { Client } from "discord.js"
 
 const client = new Client({
   intents: [
-    "Guilds",
-    "GuildMessages",
     "DirectMessages",
-    "GuildVoiceStates",
+    "Guilds",
     "GuildMembers",
+    "GuildMessages",
+    "GuildVoiceStates",
   ],
 })
 
