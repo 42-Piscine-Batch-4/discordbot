3a529b741f06dcda03add4f73f0fc95486721ac5
rbd03 <raydenleong03@gmail.com>
2024-05-10 01:42:34 +0800

    feat: find_members - add different name match types.

--- src/commands/find_members.ts
+++ src/commands/find_members.ts
@@ -12,21 +12,31 @@ export const data = new SlashCommandBuilder()
 export const execute = async (interaction: ChatInputCommandInteraction) => {
   const substr = interaction.options.getString("substring", true)
   const memList = await interaction.guild?.members.fetch()
-  const message = memList
-    ?.filter(
-      (member) =>
-        member.displayName.includes(substr) ||
-        member.user.username.includes(substr)
-    )
-    .map((member) => `<@${member.user.id}>`)
-    .join(", ")
-  const output = `Substring:\n"${substr}"\n\n`
-  if (message) {
+  const usernames =
+    memList
+      ?.filter((member) => member.user.username.includes(substr))
+      .map((member) => `<@${member.user.id}>`) ?? []
+  const nicknames =
+    memList
+      ?.filter((member) => member.nickname?.includes(substr))
+      .map((member) => `<@${member.user.id}>`)
+      .filter((member) => !usernames?.includes(member)) ?? []
+  let output = `Substring:\n"${substr}"\n\n`
+  if (usernames.length > 0) {
+    output = output + "Usernames matching:\n" + usernames.join(", ")
+  }
+  if (nicknames.length > 0) {
+    if (usernames.length > 0) {
+      output = output + "\n\n"
+    }
+    output = output + "Nicknames matching:\n" + nicknames.join(", ")
+  }
+  if (usernames.length < 1 && nicknames.length < 1) {
+    await interaction.reply(`${output}No members match the substring.`)
+  } else {
     await interaction.reply({
-      content: `${output}Members matching the substring:\n\n${message}`,
+      content: `${output}`,
       allowedMentions: { parse: [] },
     })
-  } else {
-    await interaction.reply(`${output}No members match the substring.`)
   }
 }
