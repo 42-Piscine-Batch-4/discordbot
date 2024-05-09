41364bd6a2b4ee039687c078cd0a06a8ff9855a4
Heixier <rzsiah@gmail.com>
2024-05-09 17:54:50 +0800

    manually accounted for changes in day

--- src/commands/bedtime.ts
+++ src/commands/bedtime.ts
@@ -4,15 +4,9 @@ import mydayjs from "../utils/dayjs"
 const COMMAND_NAME = "bedtime"
 
 // Sets the start and end timings here for easier access in future
-type Time = { hour: number; minute: number }
-
-const MINUTES: number = 60
-
-const startTime: Time = { hour: 1, minute: 0 }
-const endTime: Time = { hour: 16, minute: 0 }
-
-const startMinutes: number = startTime.hour * MINUTES + startTime.minute
-const endMinutes: number = endTime.minute * MINUTES + startTime.minute
+type Time = { day: number; hour: number; minute: number }
+const startTime: Time = { day: 1, hour: 0, minute: 0 }
+const endTime: Time = { day: 1, hour: 7, minute: 0 }
 
 export const data = new SlashCommandBuilder()
   .setName(COMMAND_NAME)
@@ -26,40 +20,26 @@ export const data = new SlashCommandBuilder()
   )
 
 export const execute = async (interaction: ChatInputCommandInteraction) => {
-  const currentTime = mydayjs().tz()
+  const currentTime = mydayjs()
 
   const bedTime = mydayjs()
-    .tz()
     .set("hour", startTime.hour)
     .set("minute", startTime.minute)
-    .add(startMinutes >= endMinutes ? 1 : 0, "day")
+    .add(startTime.day, "day")
 
   const wakeTime = mydayjs()
-    .tz()
     .set("hour", endTime.hour)
     .set("minute", endTime.minute)
+    .add(endTime.day, "day")
 
   const isBedTime =
-    (startMinutes <= endMinutes && currentTime.isBetween(bedTime, wakeTime)) ||
-    (startMinutes > endMinutes &&
-      (currentTime.isAfter(bedTime) ||
-        currentTime.isBefore(wakeTime.add(1, "day"))))
+    currentTime.isBetween(bedTime, wakeTime) || currentTime.isSame(bedTime)
 
   const bedHoursDiff = Math.abs(currentTime.diff(bedTime, "hours"))
-  const bedMinutesDiff = Math.abs(
-    currentTime.diff(bedTime, "minutes") % MINUTES
-  )
+  const bedMinutesDiff = Math.abs(currentTime.diff(bedTime, "minutes") % 60)
 
   const wakeHoursDiff = Math.abs(currentTime.diff(wakeTime, "hours"))
-  const wakeMinutesDiff = Math.abs(
-    currentTime.diff(wakeTime, "minutes") % MINUTES
-  )
-  console.log(
-    `bedTime: ${bedTime}\nwakeTime: ${wakeTime}\ncurrentTime: ${currentTime}\n`
-  )
-  console.log(
-    `isBedTime: ${isBedTime}\nbedHoursDiff: ${bedHoursDiff} bedMinutesDiff: ${bedMinutesDiff}\nwakeHoursDiff: ${wakeHoursDiff} wakeMinutesDiff: ${wakeMinutesDiff}\n`
-  )
+  const wakeMinutesDiff = Math.abs(currentTime.diff(wakeTime, "minutes") % 60)
 
   if (!isBedTime) {
     interaction.reply(
@@ -68,11 +48,11 @@ export const execute = async (interaction: ChatInputCommandInteraction) => {
   } else {
     if (interaction.options.getString("defy")) {
       interaction.reply(
-        `It's past ${bedTime.format("h:mm a")} but I don't care!!!`
+        `It's${currentTime.isSame(bedTime) ? "" : " past"} ${bedTime.format("h:mm a")} but I don't care!!!`
       )
     } else {
       interaction.reply(
-        `It's past ${bedTime.format("h:mm a")}. I have ${wakeHoursDiff <= 1 ? (wakeHoursDiff === 0 ? "" : wakeHoursDiff + " hour and ") : wakeHoursDiff + " hours and "}${wakeMinutesDiff} more minutes left to sleep.`
+        `It's${currentTime.isSame(bedTime) ? "" : " past"} ${bedTime.format("h:mm a")}. I have ${wakeHoursDiff <= 1 ? (wakeHoursDiff === 0 ? "" : wakeHoursDiff + " hour and ") : wakeHoursDiff + " hours and "}${wakeMinutesDiff} more minutes left to sleep.`
       )
     }
   }
